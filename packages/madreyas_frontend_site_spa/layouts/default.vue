6ff8c1594807e6462701f75a88e9f3b64bb1ad77
venkat <subash.venkat007@gmail.com>
2020-10-03 07:11:05 +0530

    fix: 🐛 dashboard display fixed

--- packages/madreyas_frontend_site_spa/layouts/default.vue
+++ packages/madreyas_frontend_site_spa/layouts/default.vue
@@ -29,8 +29,9 @@
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-
-    <nuxt />
+    <v-main>
+      <nuxt />
+    </v-main>
   </v-app>
 </template>
 
--- packages/madreyas_frontend_site_spa/pages/index.vue
+++ packages/madreyas_frontend_site_spa/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid>
-    <v-row dense>
+    <v-row>
       <v-col
         v-for="animal in this.$store.state.animals.animalsList"
         :key="animal.name"

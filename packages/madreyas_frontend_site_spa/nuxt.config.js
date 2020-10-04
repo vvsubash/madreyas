25ce6b48ec19e7369e858362385c2aa29b41cf85
venkat <subash.venkat007@gmail.com>
2020-10-04 05:52:45 +0530

    feat: 🎸 PWA Support

--- packages/madreyas_frontend_site_spa/nuxt.config.js
+++ packages/madreyas_frontend_site_spa/nuxt.config.js
@@ -12,14 +12,14 @@ export default {
   },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - madreyas_frontend_site_spa',
-    title: 'madreyas_frontend_site_spa',
+    titleTemplate: '%s - Madreyas App',
+    title: 'Madreyas App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.png' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
@@ -39,9 +39,9 @@ export default {
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
   ],
-
+  // Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [],
+  modules: ['@nuxtjs/pwa'],
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {

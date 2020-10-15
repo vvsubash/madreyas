b92fc18b2b7fe362852dfe0df7c68cb779a1c5c7
venkat <subash.venkat007@gmail.com>
2020-10-15 22:23:48 +0530

    ff

--- packages/madreyas_frontend_site_spa/components/view-animal-inseminated.vue
+++ packages/madreyas_frontend_site_spa/components/view-animal-inseminated.vue
@@ -1,19 +1,25 @@
 <template>
-  <div v-if="animal.check1.isPassed != true">
+  <h3 v-if="animal.check1.isPassed != null">
     {{
-      `Hey your ${animal.species} ${
-        animal.name
-      } has  beein Inseminated on ${$dateFns.format(
-        animal.inseminatedOn.toDate(),
-        'dd-MM-yyyy',
-      )} and waiting to see if she comes to heat on ${$dateFns.format(
-        animal.check1.date.toDate(),
-        'dd-MM-yyyy',
-      )} `
+      `Hey your ${animal.species} ${animal.name} has beein Inseminated on
+    ${$dateFns.format(animal.inseminatedOn.toDate(), 'dd-MM-yyyy')} and
+    waiting to see if she comes to heat on ${$dateFns.format(
+      animal.check1.date.toDate(),
+      'dd-MM-yyyy',
+    )} if she hasn't comes to heat then insemination is sucessfull and you can mark check as passed. `
+    }}
+  </h3>
+  <div v-else-if="animal.check2.isPassed != null">
+    {{
+      `Hey your ${animal.species} ${animal.name} has beein Inseminated on
+    ${$dateFns.format(animal.inseminatedOn.toDate(), 'dd-MM-yyyy')} and
+    you can check for pregnancy is ${$dateFns.format(
+      animal.check2.date.toDate(),
+      'dd-MM-yyyy',
+    )} if she hasn't comes to heat then insemination is sucessfull and you can mark check as passed. `
     }}
   </div>
-  <div v-else-if="animal.check2.isPassed != true">check2</div>
-  <div v-else-if="animal.check3.isPassed != true">check3</div>
+  <div v-else-if="animal.check3.isPassed != null">check3</div>
 </template>
 <script>
 export default {

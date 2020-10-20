bc70e0571d3814064f8eb6b8cc56a13285a15b77
venkat <subash.venkat007@gmail.com>
2020-10-21 05:26:09 +0530

    removed purgecss

--- packages/madreyas_frontend_site_spa/nuxt.config.js
+++ packages/madreyas_frontend_site_spa/nuxt.config.js
@@ -47,7 +47,6 @@ export default {
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
     '@nuxtjs/date-fns',
-    'nuxt-purgecss',
   ],
   // Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
   // Modules (https://go.nuxtjs.dev/config-modules)

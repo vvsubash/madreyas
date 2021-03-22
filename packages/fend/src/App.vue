06d6be8d11bf5fded1d3d3fa13263fa2593f2257
venkat <subash.venkat007@gmail.com>
2021-03-22 07:06:44 +0530

    df

--- packages/fend/src/App.vue
+++ packages/fend/src/App.vue
@@ -1,27 +1,21 @@
 <template>
+<div>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite need to add vue router" />
+  <unit-length-converter/>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import UnitLengthConverter from './components/UnitLengthConverter.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    UnitLengthConverter
   }
 })
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
